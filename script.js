88cb28e1738cd3ad9c095b5ffd76b36d6df0e540
Catrina-W <113054736+Catrina-W@users.noreply.github.com>
2024-08-12 18:58:00 -0400

    Added search bar

---
+++ script.js
@@ -0,0 +1,6 @@
+const search = () => {
+    const searchbox = document.getElementById("search-item").ariaValueMax.toUpperCase();
+    const comicitems = document.getElementById("book-container");
+    const book = document.querySelectorAll(".book")
+    const year = getElementsbyTagName
+}
